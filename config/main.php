d68e49b29f18f78bbbfb80d01e725cf15215f558
Roman <kuzmin.sto@yandex.ru>
2018-02-15 13:37:55 +0500

    something

--- config/main.php
+++ config/main.php
@@ -11,24 +11,24 @@ return [
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
-/*     'modules' => [
-    'user' => [
+    'modules' => [
+/*     'user' => [
         // following line will restrict access to profile, recovery, registration and settings controllers from backend
         'as backend' => 'dektrium\user\filters\BackendFilter',
-    ],
     ], */
+    ],
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-backend',
 			'baseUrl' => '/admin',
         ],
-        /* 'user' => [
+        'user' => [
         'identityCookie' => [
             'name'     => '_backendIdentity',
             'path'     => '/admin',
             'httpOnly' => true,
         ],
-        ], */
+        ],
         'session' => [
         'name' => 'BACKENDSESSID',
         'cookieParams' => [
